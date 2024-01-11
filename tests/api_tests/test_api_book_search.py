aec11d306f7bf15c4d79a97b5734c5d4c2728d4b
flowerfrog <alexosenn@gmail.com>
2024-01-11 16:30:50 +0300

    Added test for unsuccessful searching books

--- tests/api_tests/test_api_book_search.py
+++ tests/api_tests/test_api_book_search.py
@@ -12,7 +12,7 @@ from helper.api_requests import api_get
 @allure.tag('regress', 'api', 'normal')
 @allure.severity('normal')
 @allure.label('layer', 'api')
-def test_searching_of_book_by_title():
+def test_successful_searching_of_book_by_title():
     schema = load_schema('successful_searching_book.json')
 
     book_title = 'Семь сестер. Потерянная сестра'
@@ -28,3 +28,26 @@ def test_searching_of_book_by_title():
     assert result.json()['payload']['data'][0]['type'] == "text_book"
     assert result.json()['payload']['data'][0]['instance']['art_type'] == 0
     assert 'Семь сестер' in result.json()['payload']['data'][0]['instance']['title']
+
+
+@allure.epic('API. Search')
+@allure.label("owner", "flowerfrog")
+@allure.feature("Checking the book search on the main page")
+@allure.label('microservice', 'API')
+@allure.label('microservice', 'Search')
+@allure.tag('regress', 'api', 'normal')
+@allure.severity('normal')
+@allure.label('layer', 'api')
+def test_unsuccessful_searching_of_book_by_title():
+    schema = load_schema('unsuccessful_searching_book.json')
+
+    book_title = 'nbmcgfhmghm'
+    types = 'text_book'
+    url = f"/search?q={book_title}&types={types}"
+    headers = {"Content-Type": "application/json"}
+
+    result = api_get(url, headers=headers)
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+    assert len(result.json()['payload']['data']) == 0

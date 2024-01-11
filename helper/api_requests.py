bf7db1a2d72df0a06c25845df5b7a2b4e849fc2f
flowerfrog <alexosenn@gmail.com>
2024-01-11 15:08:50 +0300

    Updated url for api requests

--- helper/api_requests.py
+++ helper/api_requests.py
@@ -7,7 +7,7 @@ import allure
 
 def api_get(url, **kwargs):
     with allure.step("API Request"):
-        result = requests.get(url="https://api.litres.ru" + url, **kwargs)
+        result = requests.get(url="https://api.litres.ru/foundation/api" + url, **kwargs)
         allure.attach(body=result.request.method + " " + result.request.url, name="Request",
                       attachment_type=AttachmentType.TEXT, extension="txt")
         allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response",
@@ -20,7 +20,7 @@ def api_get(url, **kwargs):
 
 def api_post(url, **kwargs):
     with allure.step("API Request"):
-        result = requests.post(url="https://api.litres.ru" + url, **kwargs)
+        result = requests.post(url="https://api.litres.ru/foundation/api" + url, **kwargs)
         allure.attach(body=result.request.method + " " + result.request.url, name="Request",
                       attachment_type=AttachmentType.TEXT, extension="txt")
         allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response",

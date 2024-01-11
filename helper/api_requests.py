d78e785cc0f7b0550125028c95bc86b41d1479a2
flowerfrog <alexosenn@gmail.com>
2024-01-11 16:21:33 +0300

    Added put api requests

--- helper/api_requests.py
+++ helper/api_requests.py
@@ -29,3 +29,16 @@ def api_post(url, **kwargs):
         logging.info(result.status_code)
         logging.info(result.text)
         return result
+
+
+def api_put(url, **kwargs):
+    with allure.step("API Request"):
+        result = requests.put(url="https://api.litres.ru/foundation/api" + url, **kwargs)
+        allure.attach(body=result.request.method + " " + result.request.url, name="Request",
+                      attachment_type=AttachmentType.TEXT, extension="txt")
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response",
+                      attachment_type=AttachmentType.JSON, extension="json")
+        logging.info(result.request.url)
+        logging.info(result.status_code)
+        logging.info(result.text)
+        return result

6030b2ef1c509af1f0f780c5d37ca56aea36a4e7
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-13 11:49:10 +0300

    Added mobile tests for android

---
+++ tests/mobile_tests/android/test_mobile_searching_book.py
@@ -0,0 +1,47 @@
+import allure
+from pages.mobile_pages.main_page import AndroidMainPage
+from pages.mobile_pages.books_to_read_page import AndroidSearchBookPage
+
+
+@allure.epic('Search')
+@allure.label("owner", "flowerfrog")
+@allure.feature("Checking the book search in mobile app")
+@allure.severity('normal')
+@allure.label('layer', 'mobile')
+def test_successful_searching_book(android_mobile_management):
+    main_page = AndroidMainPage()
+    book_page = AndroidSearchBookPage()
+
+    with allure.step('Selecting the application language'):
+        main_page.selecting_application_language()
+
+    with allure.step('Hiding the adult content'):
+        main_page.hiding_adult_content()
+
+    with allure.step('Type search'):
+        book_page.searching_book()
+
+    with allure.step('Verify content found'):
+        book_page.book_must_be_found()
+
+
+@allure.epic('Search')
+@allure.label("owner", "flowerfrog")
+@allure.feature("Checking the book search in mobile app")
+@allure.severity('normal')
+@allure.label('layer', 'mobile')
+def test_unsuccessful_searching_book(android_mobile_management):
+    main_page = AndroidMainPage()
+    book_page = AndroidSearchBookPage()
+
+    with allure.step('Selecting the application language'):
+        main_page.selecting_application_language()
+
+    with allure.step('Hiding the adult content'):
+        main_page.hiding_adult_content()
+
+    with allure.step('Type search'):
+        book_page.searching_non_existent_book()
+
+    with allure.step('Verify content not found'):
+        book_page.book_must_not_be_found()

36f0645258b35dbc4df1673f3411f2a16a04ade8
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-13 11:46:17 +0300

    Added mobile pages

---
+++ pages/mobile_pages/main_page.py
@@ -0,0 +1,28 @@
+from selene import browser
+from appium.webdriver.common.appiumby import AppiumBy
+
+
+class AndroidMainPage:
+
+    def selecting_application_language(self):
+        browser.element((AppiumBy.ID, "ru.litres.android:id/title")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).click()
+        return self
+
+    def hiding_adult_content(self):
+        browser.element((AppiumBy.ID, "ru.litres.android:id/btnDisableAdultContent")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android:id/btnConfirmDisableAdultContent")).click()
+        return self
+
+
+class IosMainPage:
+
+    def selecting_application_language(self):
+        browser.element((AppiumBy.ID, "ru.litres.android:id/title")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).click()
+        return self
+
+    def hiding_adult_content(self):
+        browser.element((AppiumBy.ID, "ru.litres.android:id/btnDisableAdultContent")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android:id/btnConfirmDisableAdultContent")).click()
+        return self

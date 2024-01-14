02cb37257bbb950e6bf699b24b241ab78a7fd126
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-14 11:42:42 +0300

    Updated book page

--- pages/mobile_pages/book_page.py
+++ pages/mobile_pages/book_page.py
@@ -1,4 +1,4 @@
-from selene import browser, have
+from selene import browser, have, be
 from appium.webdriver.common.appiumby import AppiumBy
 
 
@@ -16,4 +16,15 @@ class AndroidBookPage:
     def book_must_be_added_to_saved(self):
         (browser.element((AppiumBy.ID, "ru.litres.android:id/bookName"))
          .should(have.text("The Adventures of Tom Sawyer")))
+        return self
+
+    def removing_book_from_saved(self):
+        browser.element((AppiumBy.ID, "ru.litres.android:id/bookName")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android:id/bookPostpone")).click()
+        return self
+
+    def book_must_be_removed_from_saved(self):
+        results = browser.all((AppiumBy.ID, 'ru.litres.android:id/empty_view'))
+        results.should(have.size(1))
+        browser.element((AppiumBy.ID, 'ru.litres.android:id/action_button')).should(be.visible)
         return self

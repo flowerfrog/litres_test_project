024c69bf9cc9fd1796aa1d6d010fb9baee0d1771
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-14 10:13:46 +0300

    Removing IosMainPage class from main page

--- pages/mobile_pages/main_page.py
+++ pages/mobile_pages/main_page.py
@@ -13,16 +13,3 @@ class AndroidMainPage:
         browser.element((AppiumBy.ID, "ru.litres.android:id/btnDisableAdultContent")).click()
         browser.element((AppiumBy.ID, "ru.litres.android:id/btnConfirmDisableAdultContent")).click()
         return self
-
-
-class IosMainPage:
-
-    def selecting_application_language(self):
-        browser.element((AppiumBy.ID, "ru.litres.android:id/title")).click()
-        browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).click()
-        return self
-
-    def hiding_adult_content(self):
-        browser.element((AppiumBy.ID, "ru.litres.android:id/btnDisableAdultContent")).click()
-        browser.element((AppiumBy.ID, "ru.litres.android:id/btnConfirmDisableAdultContent")).click()
-        return self

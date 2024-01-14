9e0e1b953d84ed7ef8805fb9e6ebfc658002487e
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-14 11:43:10 +0300

    Added test for removing book from Saved

---
+++ tests/mobile_tests/android/test_mobile_remove_book_from_saved.py
@@ -0,0 +1,42 @@
+import allure
+from pages.mobile_pages.main_page import AndroidMainPage
+from pages.mobile_pages.books_to_read_page import AndroidSearchBookPage
+from pages.mobile_pages.book_page import AndroidBookPage
+
+
+@allure.epic('Remove book to saved')
+@allure.label("owner", "flowerfrog")
+@allure.feature("Checking whether a book has been removed from saved in mobile app")
+@allure.severity('normal')
+@allure.label('layer', 'mobile')
+def test_removing_book_to_saved(android_mobile_management):
+    main_page = AndroidMainPage()
+    book_search_page = AndroidSearchBookPage()
+    book_page = AndroidBookPage()
+
+    with allure.step('Selecting the application language'):
+        main_page.selecting_application_language()
+
+    with allure.step('Hiding the adult content'):
+        main_page.hiding_adult_content()
+
+    with allure.step('Type search'):
+        book_search_page.searching_book()
+
+    with allure.step('Choosing a book'):
+        book_search_page.choosing_book()
+
+    with allure.step('Adding a book to Saved'):
+        book_page.adding_book_to_saved()
+
+    with allure.step('Go to the Saved tab'):
+        book_page.go_to_saved_tab()
+
+    with allure.step('Removing a book from Saved'):
+        book_page.removing_book_from_saved()
+
+    with allure.step('Go to the Saved tab'):
+        book_page.go_to_saved_tab()
+
+    with allure.step('Checking that the selected book has been removed from Saved'):
+        book_page.book_must_be_removed_from_saved()

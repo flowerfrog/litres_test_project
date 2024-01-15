23e5de91707a236f113c9039175a9ed3c041aaa8
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-15 09:26:13 +0300

    Added fixture load_env

--- tests/web_tests/conftest.py
+++ tests/web_tests/conftest.py
@@ -16,6 +16,11 @@ def pytest_addoption(parser):
     )
 
 
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
